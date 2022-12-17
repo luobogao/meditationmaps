7109add069d027a1177176d9b7a2e52362d9e5e4
Steffan <steffan.iverson@gmail.com>
2022-12-16 20:38:25 -0800

    sidebar

--- js/code.js
+++ js/code.js
@@ -1,27 +1,21 @@
-import { w2sidebar } from 'js/w2ui.js'
 
-new w2sidebar({
-    box: '#sidebar',
-    name: 'sidebar',
-    nodes: [
-        { id: 'level-1', text: 'Level 1', expanded: true, group: true,
-            nodes: [
-                { id: 'level-1-1', text: 'Level 1.1', icon: 'fa fa-home' },
-                { id: 'level-1-2', text: 'Level 1.2', icon: 'fa fa-star' },
-                { id: 'level-1-3', text: 'Level 1.3', icon: 'fa fa-star-o' }
-            ]
-        },
-        { id: 'level-2', text: 'Level 2', expanded: true, group: true,
-            nodes: [
-                { id: 'level-2-1', text: 'Level 2.1', icon: 'fa fa-star', count: 3,
-                    nodes: [
-                        { id: 'level-2-1-1', text: 'Level 2.1.1', icon: 'fa fa-star-o' },
-                        { id: 'level-2-1-2', text: 'Level 2.1.2', icon: 'fa fa-star-o', count: 67 },
-                        { id: 'level-2-1-3', text: 'Level 2.1.3', icon: 'fa fa-star-o' }
-                    ]},
-                { id: 'level-2-2', text: 'Level 2.2', icon: 'fa fa-star-o' },
-                { id: 'level-2-3', text: 'Level 2.3', icon: 'fa fa-star-o' }
-            ]
-        }
-    ]
-})
+const fontFamily = "Roboto, sans-serif"
+const textSizeLarge = 25
+const textSizeMed = 18
+const textSizeSmall = 12
+
+
+function receivedFile()
+{
+    d3.select ("#save-btn").style("display", "flex") 
+    console.log ("REVEIVED DATA")
+    let string = fr.result
+    let data = d3.csvParse (string)
+    let headers = data.slice (-1) [0]
+    let rows = data.slice (0, data.length - 1)
+    let keys = Object.keys (rows [0])
+ 
+
+}
+
+buildBrowseFile(d3.select("#main"), "test", "t1")
---
+++ js/ui.js
@@ -0,0 +1,76 @@
+
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("open_btn").style.display = "none"
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("open_btn").style.display = "flex"
+    document.getElementById("main").style.marginLeft= "0";
+  }
+function buildBrowseFile (div, label, id)
+{
+    let holder = div.append ("div")
+        .style ("position", "relative")
+        .attr ("font-family", fontFamily)
+        .attr ("font-size", textSizeMed + "px")
+        .attr ("id", id + "-all")
+        .style ("width", "150px")
+
+    holder
+        .append ("input")
+        .style ("position", "relative")
+        .style ("text-align", "right")
+        .style ("opacity", 0)
+        .style ("z-index", 2)
+        .attr ("class", "browse-id")
+        .style ("width", "150px")
+        .attr ("type", "file")
+        .on ("mouseover", function(d) {
+            d3.select(this).style("cursor", "pointer");
+            d3.select ("#" + id)
+                .style ("background", "grey")
+                .style ("border-radius", "5px")
+
+        })
+
+        .on ("mouseout", function(d) {
+            d3.select(this).style("cursor", "default");
+            d3.select ("#" + id)
+                .style ("border-radius", "5px")
+                .style ("background", "#f0f0f0")
+        })
+
+        .on ("change", function (evt){
+            document.getElementById (id).click ()
+            
+            let file = evt.target.files [0]
+
+            fr = new FileReader ()
+            fr.onload = receivedFile
+            fr.readAsText (file)
+
+
+
+
+        })
+
+
+    let fakefile = holder.append ("div")
+        .style ("position", "absolute")
+        .style ("top", "0px")
+        .style ("left", "0px")
+        .style ("z-index", 1)
+
+    let btn = fakefile.append ("button")
+        .style ("font-size", "18px")
+        .attr ("font-family", fontFamily)
+        .attr("color", "#ececf1ff")
+    //.style ("height", "30px")
+        .attr ("id", id)
+        .text (label)
+
+}
